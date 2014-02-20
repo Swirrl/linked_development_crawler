c549c19ea8f12ffdc484fbcb937ed6f1fbc9c11c
Rick Moynihan <rick.m@swirrl.com>
2014-02-20 10:12:05 +0000

    Install zip in Dockerfile.

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+function fail_fast {
+    echo "Failed to bootstrap" 1>&2;
+    exit 1
+}
+
+trap 'fail_fast' ERR 
+
+python /vagrant//bootstrap/bootstrap_eldis.py
+python /vagrant/bootstrap/bootstrap_r4d.py
